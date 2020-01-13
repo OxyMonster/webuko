6146ef6b607d698c505b7d8c02cde3eca430a1a2
oxymonster <giorgizeikidze97@gmail.com>
2020-01-13 04:02:21 +0400

    $$$$

--- src/app/home/slider/slider.component.ts
+++ src/app/home/slider/slider.component.ts
@@ -9,13 +9,19 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class SliderComponent implements OnInit {
 
+  isHome: boolean = false; 
+
   constructor(
     private router: Router,
     private modalService: NgbModal
   ) { }
 
   ngOnInit() {
-
+    this.router.url === '/home' ? this.isHome = true : this.isHome = false; 
+    console.log(this.router.url);
+    console.log(this.isHome);
+    
+    
   }
 
   onNavigate() {
